d44cc3520656699e44f77906b7e8e4233beb3cc5
David Lafleur <github@davlaf.com>
2024-03-24 20:39:51 -0400

    visualize

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
-import tables
+import pandas as pd
+
+import matplotlib.pyplot as plt
 
 
-def store_raw_file_in_hdf5(csv_file_path, hdf5_file_path, hdf5_node_path):
-    # Open or create the HDF5 file
-    with tables.open_file(hdf5_file_path, mode='a') as h5file:
-        # Read the CSV file as binary data
-        with open(csv_file_path, 'rb') as f:
-            csv_binary_data = f.read()
+def main():
+    jump_csv_file_path = 'jump_back_pocket.csv'
+    walk_csv_file_path = 'walk_back_pocket.csv'
 
-        # Correctly identify the node path for checking existence and removal
-        # Ensure hdf5_node_path does not refer to the root node and is correctly formatted
-        if hdf5_node_path.startswith('/'):
-            full_path = hdf5_node_path
-        else:
-            full_path = '/' + hdf5_node_path  # Ensure the path is absolute
+    data_jump = pd.read_csv(jump_csv_file_path)
+    data_walk = pd.read_csv(walk_csv_file_path)
 
-        # Check if the node exists
-        if h5file.__contains__(full_path):
-            # Remove the node if it exists
-            h5file.remove_node(full_path, recursive=True)
+    # Create a 3D plot
+    fig = plt.figure()
+    gs = fig.add_gridspec(2,2, wspace=0.5, hspace=0.5)
+    ax = fig.add_subplot(gs[:, 0], projection='3d')
 
-        # Now, safely proceed to recreate and store data in the node
-        atom = tables.UInt8Atom()
-        filters = tables.Filters(complevel=5, complib='blosc')
-        array_c = h5file.create_carray(h5file.root, hdf5_node_path.strip('/'), atom, shape=(len(csv_binary_data),),
-                                       filters=filters)
-        array_c[:] = bytearray(csv_binary_data)
+    jumping, = ax.plot(data_jump['x'], data_jump['y'], data_jump['z'], color='r')
+    jumping.set_label("Jumping")
+    walking, = ax.plot(data_walk['x'], data_walk['y'], data_walk['z'], color='g')
+    walking.set_label("Walking")
+
+    ax.legend()
+    # Set labels and title
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('Directional acceleration')
+
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax2.plot(data_jump['time'], data_jump['abs'], color='r')
+    ax2.set_ylim([-5, 100])
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Abs. accel. (m/s^2)')
+    ax2.set_title('Jumping abs. acceleration')
+
+    ax3 = fig.add_subplot(gs[1, 1])
+    ax3.plot(data_walk['time'], data_walk['abs'], color='g')
+    ax3.set_ylim([-5, 100])
+    ax3.set_xlabel('Time (s)')
+    ax3.set_ylabel('Abs. accel. (m/s^2)')
+    ax3.set_title('Walking abs. acceleration')
+
+    # Show plot
+    plt.show()
 
 
-# Example usage
-csv_file_path = 'noah_data.csv'
-hdf5_file_path = 'your_output_file.h5'
-hdf5_node_path = 'raw_csv_storage'  # Ensure this is not root or improperly formatted
-
-store_raw_file_in_hdf5(csv_file_path, hdf5_file_path, hdf5_node_path)
+if __name__ == "__main__":
+    main()
